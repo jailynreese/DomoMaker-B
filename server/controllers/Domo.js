46633101ad6e7aae39f8c6fcef370adf1074f864
jailynreese <jreeset@gmail.com>
2021-10-31 23:21:40 -0400

    initial commit

---
+++ server/controllers/Domo.js
@@ -0,0 +1,39 @@
+const models = require('../models');
+
+const Domo = models.Domo;
+
+const makeDomo = (req, res) => {
+  if(!req.body.name || !req.body.age){
+    return res.status(400).json({error: 'RAWR! Both name and age are required.'});
+  }
+
+  const domoData = {
+    name: req.body.name,
+    age: req.body.age,
+    owner: req.session.account._id,
+  };
+
+  const newDomo = new Domo.DomoModel(domoData);
+
+  const domoPromise = newDomo.save();
+
+  domoPromise.then(() => res.json({ redirect: '/maker'}));
+
+  domoPromise.catch((err) => {
+    console.log(err);
+    if(err.code === 11000) {
+      return res.status(400).json({ error: 'Domo already exists. '});
+    }
+
+    return res.status(400).json({ error: 'An error occurred' });
+  });
+
+  return domoPromise;
+};
+
+const makerPage = (req, res) => {
+  res.render('app');
+};
+
+module.exports.makerPage = makerPage;
+module.exports.make = makeDomo;
